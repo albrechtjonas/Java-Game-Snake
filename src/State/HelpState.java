2b513036603d4c9cc0190f7380f4bd26259c162a
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-14 16:58:34 -0700

    Update HelpState.java

--- src/State/HelpState.java
+++ src/State/HelpState.java
@@ -1,1 +1,55 @@
+package State;
 
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+import Entity.Button.Button;
+import Entity.Label.Label;
+import Enum.Action;
+import Main.Display;
+
+public class HelpState extends State {
+	
+	private Label help;
+	
+	private Label one;
+	
+	private Label two;
+	
+	private Label three;
+	
+	private Button back;
+	
+	public HelpState(Display display) {
+		super(display);
+		
+		createObject();
+	}
+	
+	private void createObject() {
+		help=new Label(display,0,0,display.getWidth(),200,"Help",new Font("TimesRoman",Font.ITALIC,150));
+		
+		one=new Label(display,0,250,display.getWidth(),100,"Use W,S,A,D to move the snake",new Font("TimesRoman",Font.ITALIC,50));
+		
+		two=new Label(display,0,400,display.getWidth(),100,"Press Spacebar to pause the game",new Font("TimesRoman",Font.ITALIC,50));
+		
+		three=new Label(display,0,550,display.getWidth(),100,"Try to get as long as possible",new Font("TimesRoman",Font.ITALIC,50));
+		
+		back=new Button(display,0,700,display.getWidth(),100,"Back",new Font("TimesRoman",Font.ITALIC,75),Action.start);
+	}
+	
+	public void tick() {
+		if(tickDelta<=0) {
+			back.tick();
+		}
+		tickDelta-=0.1;
+	}
+	
+	public void render(Graphics2D g) {
+		help.render(g);
+		one.render(g);
+		two.render(g);
+		three.render(g);
+		back.render(g);
+	}
+}
