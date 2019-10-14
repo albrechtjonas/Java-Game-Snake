bb2271866e26dc4dd77a4a3a4b0470dfcd938641
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-14 16:58:19 -0700

    Update EndState.java

--- src/State/EndState.java
+++ src/State/EndState.java
@@ -1,1 +1,53 @@
+package State;
 
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+import Entity.Button.Button;
+import Entity.Label.Label;
+import Enum.Action;
+import Main.Display;
+
+public class EndState extends State {
+	
+	private Label gameOver;
+	
+	private Label score;
+	
+	private Button restart;
+	
+	private Button menu;
+
+	public EndState(Display display) {
+		super(display);
+		
+		createObject();
+	}	
+	
+	private void createObject() {
+		gameOver=new Label(display,0,0,display.getWidth(),200,"Game Over",new Font("TimesRoman",Font.ITALIC,150));
+		
+		int length=display.getRunState().getSnake().getBody().size();
+		
+		score=new Label(display,0,300,display.getWidth(),100,"Score "+length,new Font("TimesRoman",Font.ITALIC,75));
+		
+		restart=new Button(display,0,500,display.getWidth(),100,"Restart",new Font("TimesRoman",Font.ITALIC,75),Action.restart);
+		
+		menu=new Button(display,0,700,display.getWidth(),100,"Menu",new Font("TimesRoman",Font.ITALIC,75),Action.start);	
+	}
+	
+	public void tick() {
+		if(tickDelta<=0) {
+			restart.tick();
+			menu.tick();
+		}
+		tickDelta-=0.1;
+	}
+
+	public void render(Graphics2D g) {
+		gameOver.render(g);
+		score.render(g);
+		restart.render(g);
+		menu.render(g);
+	}
+}
