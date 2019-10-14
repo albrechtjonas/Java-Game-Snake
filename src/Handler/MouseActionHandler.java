43ea0d6b71cd672ec064ade0511691de21db04b7
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-14 16:55:57 -0700

    Update MouseActionHandler.java

--- src/Handler/MouseActionHandler.java
+++ src/Handler/MouseActionHandler.java
@@ -1,1 +1,42 @@
+package Handler;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseActionHandler implements MouseListener {
+	
+	private boolean pressed;
+	
+	public MouseActionHandler() {
+		
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		pressed=true;
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		pressed=false;
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean getPressed() {
+		return pressed;
+	}
+}
