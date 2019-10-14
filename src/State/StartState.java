a1b22be22ba3b6db37471c0e551b700f3a568366
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-14 16:59:30 -0700

    Update StartState.java

--- src/State/StartState.java
+++ src/State/StartState.java
@@ -1,1 +1,52 @@
+package State;
 
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+import Entity.Button.Button;
+import Entity.Label.Label;
+import Enum.Action;
+import Main.Display;
+
+public class StartState extends State {
+	
+	private Label title;
+	
+	private Button play;
+	
+	private Button help;
+	
+	private Button about;
+	
+	public StartState(Display display) {
+		super(display);
+		
+		createObject();
+	}
+	
+	private void createObject() {
+		title=new Label(display,0,0,display.getWidth(),200,"Snake",new Font("TimesRoman",Font.ITALIC,150));
+		
+		play=new Button(display,0,300,display.getWidth(),100,"Play",new Font("TimesRoman",Font.ITALIC,75),Action.run);
+		
+		help=new Button(display,0,500,display.getWidth(),100,"Help",new Font("TimesRoman",Font.ITALIC,75),Action.help);
+		
+		about=new Button(display,0,700,display.getWidth(),100,"About",new Font("TimesRoman",Font.ITALIC,75),Action.about);
+	}
+	
+	public void tick() {
+		if(tickDelta<=0) {
+		play.tick();
+		help.tick();
+		about.tick();
+		}
+		tickDelta-=0.1;
+	}
+	
+	public void render(Graphics2D g) {
+		title.render(g);
+		play.render(g);
+		help.render(g);
+		about.render(g);
+	}
+}
