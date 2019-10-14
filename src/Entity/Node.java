b6da4def5ff06897aa20f5d53a298f96aaa15e60
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-14 16:54:46 -0700

    Update Node.java

--- src/Entity/Node.java
+++ src/Entity/Node.java
@@ -1,1 +1,18 @@
+package Entity;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import Interface.Renderable;
+import Main.Display;
+
+public class Node extends Entity implements Renderable {
+	public Node(Display display,int x,int y) {
+		super(display,x,y);
+	}
+	
+	public void render(Graphics2D g) {
+		g.setColor(Color.GREEN);
+		g.fillRect(x,y,display.getResolution(),display.getResolution());
+	}
+}
