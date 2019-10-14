06b693f94fec755caf251806324e5c691aaa7634
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-14 16:57:23 -0700

    Update Snake.java

--- src/Main/Snake.java
+++ src/Main/Snake.java
@@ -1,1 +1,24 @@
+package Main;
 
+public class Snake {
+	
+	private final String title="Snake";
+	
+	private final int width=820;
+	
+	private final int height=820;
+	
+	public Window window;
+	
+	private Display display;
+	
+	public static void main(String[]args) {
+		new Snake();
+	}
+	
+	private Snake() {
+		window=new Window(title,width,height);
+		display=new Display(window);
+		display.start();
+	}
+}
